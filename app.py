ba80186f42c5be567b0a261e8764781dac4e1a06
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2022-02-07 17:22:09 -0300

    ✨ CV - 09

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ from PIL import Image
 
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
+st.write('''
+# Lucas Alves - Estudante de Ciência de dados & Fan de Python.
+##### *Resume* 
+''')
+
+image = Image.open('cv.png')
+st.image(image, width=150)
